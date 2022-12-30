2bd58541ba6bb68f02bc66cf2a3625814d8d2a84
Siham <siham@angularjs.com>
2022-12-30 05:09:42 -0800

    Stack

---
+++ stack.py
@@ -0,0 +1,51 @@
+max_size =5
+class stack :
+    
+    def __init__(self):
+        self.top =-1
+        self.items = [None]*max_size
+    
+    
+    def isEmpty(self):
+        return self.top<0
+
+    def push(self,element):
+        if self.top>=max_size-1:
+            print('the stack on push')
+        else :
+            self.top +=1
+            self.items[self.top]=element
+    
+    def pop(self):
+        if self.isEmpty():
+            print('the stack is Empty')
+        else:
+            self.top-=1
+
+    def getTop(self):
+        if self.isEmpty():
+            print('the stack is empty')
+        else : 
+            stack_top = self.items[self.top]
+            print(stack_top)
+
+    def displayStack(self):
+        print('[',end="")
+        for i in range (self.top,-1,-1):
+            print(self.items[i], end="")
+        print("]")
+
+
+s = stack()
+
+s.push(1)
+s.push(2)
+s.push(3)
+s.push(4)
+
+s.displayStack()
+
+
+    
+            
+
---
+++ stackPointer.py
@@ -0,0 +1,55 @@
+class StackNode:
+    def __init__(self, item, next=None):
+        self.item = item
+        self.next = next
+
+class Stack:
+    def __init__(self):
+        self.top_ptr = None
+
+    def is_empty(self):
+        return self.top_ptr is None
+
+    def push(self, new_item):
+        new_ptr = StackNode(new_item, self.top_ptr)
+        self.top_ptr = new_ptr
+
+    def pop(self):
+        if self.is_empty():
+            print("Stack empty on pop")
+        else:
+            temp = self.top_ptr
+            self.top_ptr = self.top_ptr.next
+            temp.next = None
+            del temp
+
+    def pop_and_return(self):
+        if self.is_empty():
+            print("Stack empty on pop")
+            return None
+        else:
+            stack_top = self.top_ptr.item
+            temp = self.top_ptr
+            self.top_ptr = self.top_ptr.next
+            temp.next = None
+            del temp
+            return stack_top
+
+    def get_top(self):
+        if self.is_empty():
+            print("Stack empty on getTop")
+        else:
+            stack_top = self.top_ptr.item
+            print("Top Element of the stack is", stack_top)
+
+    def display(self):
+        cur_ptr = self.top_ptr
+        print("Items in the stack : [", end=" ")
+        while cur_ptr is not None:
+            print(cur_ptr.item, end=" ")
+            cur_ptr = cur_ptr.next
+        print("]")
+
+s = Stack()
+s.push(10)
+s.display()
